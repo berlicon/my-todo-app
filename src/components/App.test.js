ccb46a584a272e98be6a277040d0ed1b18a1a8dd
MOSCOW\Konstantin_Berlinski <Konstantin_Berlinskii@epam.com>
2017-04-14 18:28:28 +0300

    fix tests - add materialUI MuiThemeProvider

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -6,17 +6,20 @@ import reducer from '../reducers'
 import App from './App';
 import About from './About'
 import { Router, Route, /*Link,*/ browserHistory } from 'react-router'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 it('<App/> component renders without crashing', () => {
   let store = createStore(reducer, window['devToolsExtension'] ? window['devToolsExtension']() : f => f)
   const div = document.createElement('div');
   ReactDOM.render(
-    <Provider store={store}>
-      <Router history={browserHistory}>
-        <Route path="/" component={App}/>
-        <Route path="/about" component={About}/>
-        <Route path="/home" component={App}/>
-        <Route path="/categories/:id" component={App}/>
-      </Router>
-    </Provider>, div);
+    <MuiThemeProvider>
+      <Provider store={store}>
+        <Router history={browserHistory}>
+          <Route path="/" component={App}/>
+          <Route path="/about" component={About}/>
+          <Route path="/home" component={App}/>
+          <Route path="/categories/:id" component={App}/>
+        </Router>
+      </Provider>
+    </MuiThemeProvider>, div);
 });
